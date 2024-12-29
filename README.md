52a60bc1d2521566a38bfd27ea4f044e81747c92
juliofhernandez <juliofernando1c@gmail.com>
2024-12-29 22:18:55 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# juliofhernandez-springboot-servicio-users
---
+++ src/main/java/com/microservices/users/SpringbootServicioUsersApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootServicioUsersApplication.class, args);
+    }
+
+}
